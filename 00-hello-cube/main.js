2990a0121bd5fe7f046e17a90f32b2c7ac2ba653
Ludger Steens <ludger.steens@gmail.com>
2023-12-22 17:00:09 +0100

    Copy current project to 00-hello-cube.

---
+++ 00-hello-cube/main.js
@@ -0,0 +1,63 @@
+import { InputHandler } from './InputHandler.js';
+import { Scene } from './Scene.js';
+import { Renderer } from './Renderer.js';
+
+function displayErrorMessage(message) {
+    const errorElement = document.getElementById("error-message");
+    errorElement.style.display = "block";
+    errorElement.textContent = message;
+}
+
+async function initGpuDevice() {
+    // 1: request adapter and device
+    if (!navigator.gpu) {
+        displayErrorMessage();
+        throw Error('WebGPU not supported.');
+    }
+
+    const adapter = await navigator.gpu.requestAdapter();
+    if (!adapter) {
+        throw Error('Couldn\'t request WebGPU adapter.');
+    }
+
+    const device = await adapter.requestDevice();
+    return device;
+}
+
+async function initDrawingContext(gpuDevice) {
+    // 2: Get reference to the canvas to render on
+    const canvas = document.querySelector('#gpu-canvas');
+    const context = canvas.getContext('webgpu');
+
+    context.configure({
+        device: gpuDevice,
+        format: navigator.gpu.getPreferredCanvasFormat(),
+        alphaMode: 'premultiplied'
+    });
+
+    return context
+}
+
+async function main() {
+    try {
+        const gpuDevice = await initGpuDevice();
+        const drawingContext = await initDrawingContext(gpuDevice);
+
+        const scene = new Scene();
+        const renderer = new Renderer(gpuDevice, scene);
+        const inputHandler = new InputHandler(drawingContext.canvas)
+
+        await renderer.init()
+        setInterval(() => renderer.renderFrame(drawingContext), 16);
+        setInterval(() => {
+            const inputState = inputHandler.getInputState();
+            scene.updateScene(inputState)
+        }, 10);
+    } catch (e) {
+        displayErrorMessage(e)
+        document.getElementById("main-div").style.display = 'none';
+        throw e;
+    }
+}
+
+main();
