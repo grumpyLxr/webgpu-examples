115a697ca6843c1d5d6607e7c12867a60bb60636
Ludger Steens <ludger.steens@gmail.com>
2023-12-22 16:58:01 +0100

    Show an error message when weggpu is not supported.

--- xx-current/main.js
+++ xx-current/main.js
@@ -2,9 +2,16 @@ import { InputHandler } from './InputHandler.js';
 import { Scene } from './Scene.js';
 import { Renderer } from './Renderer.js';
 
+function displayErrorMessage(message) {
+    const errorElement = document.getElementById("error-message");
+    errorElement.style.display = "block";
+    errorElement.textContent = message;
+}
+
 async function initGpuDevice() {
     // 1: request adapter and device
     if (!navigator.gpu) {
+        displayErrorMessage();
         throw Error('WebGPU not supported.');
     }
 
@@ -32,20 +39,25 @@ async function initDrawingContext(gpuDevice) {
 }
 
 async function main() {
-    const gpuDevice = await initGpuDevice();
-    const drawingContext = await initDrawingContext(gpuDevice);
+    try {
+        const gpuDevice = await initGpuDevice();
+        const drawingContext = await initDrawingContext(gpuDevice);
 
-    const scene = new Scene();
-    const renderer = new Renderer(gpuDevice, scene);
-    const inputHandler = new InputHandler(drawingContext.canvas)
+        const scene = new Scene();
+        const renderer = new Renderer(gpuDevice, scene);
+        const inputHandler = new InputHandler(drawingContext.canvas)
 
-    await renderer.init()
-    setInterval(() => renderer.renderFrame(drawingContext), 16);
-
-    setInterval(() => {
-        const inputState = inputHandler.getInputState();
-        scene.updateScene(inputState)
-    }, 10);
+        await renderer.init()
+        setInterval(() => renderer.renderFrame(drawingContext), 16);
+        setInterval(() => {
+            const inputState = inputHandler.getInputState();
+            scene.updateScene(inputState)
+        }, 10);
+    } catch (e) {
+        displayErrorMessage(e)
+        document.getElementById("main-div").style.display = 'none';
+        throw e;
+    }
 }
 
 main();
